2f2d52f020c38441f5dfa391e82b21dd8484d492
sylwiavargas <sylwia.vargas@gmail.com>
2020-08-13 19:00:44 +0200

    before lecture

---
+++ mod1-lectures/15-AR-relationships/starter_kit/db/seeds.rb
@@ -0,0 +1,14 @@
+basil = Plant.create(name: "basil the herb", bought: 20200610, color: "green")
+
+corn_tree = Plant.create(name: "Corn Tree", bought: 20170203, color: "green")
+prayer_plant = Plant.create(name: "Prayer plant", bought: 20190815, color: "purple")
+cactus_1 = Plant.create(name: "Cactus", bought: 20200110, color: "ugly green")
+elephant_bush = Plant.create(name: "Elephant bush", bought: 20180908, color: "green")
+photos = Plant.create(name: "Photos", bought: 20170910, color: "green")
+dragon_tree = Plant.create(name: "Dragon tree", bought: 20170910, color: "green")
+snake_plant = Plant.create(name: "Snake plant", bought: 20170910, color: "dark green")
+polka_dot_plant = Plant.create(name: "polka dot plant", bought: 20170915, color: "pink and green")
+cactus_2 = Plant.create(name: "Cactus", bought: 20200517, color: "green")
+
+joel = Person.create(name: "Joel")
+sylwia = Person.create(name: "Sylwia")
