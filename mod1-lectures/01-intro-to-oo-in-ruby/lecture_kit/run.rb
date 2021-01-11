9f824acbce8e419a2f9a7d9cb7308f3c665099d6
sylwiavargas <sylwia.vargas@gmail.com>
2021-01-11 13:13:04 -0500

    adds better folder structure

--- mod1-lectures/01-intro-to-oo-in-ruby/lecture_kit/run.rb
+++ mod1-lectures/01-intro-to-oo-in-ruby/lecture_kit/run.rb
@@ -10,6 +10,11 @@ class String
         short_words_arr = %w{a an and as at but by for if in of on or the to via vs vs.}
     
         title = split.map do |word|
+            # lines 18-22 could be refactored using ternaries:
+            ## short_words_arr.include?(word) ? word : word.capitalize
+            # or one-liner if/unless statement, in which case we need to return the word:
+            ## word.capitalize! unless short_words_arr.include?(word)
+            ## word
             if short_words_arr.include?(word)
                 word
             else 
@@ -19,9 +24,6 @@ class String
         title.join(" ")
     end
 end
-
-
-
 
 
 
---
+++ mod1-lectures/01-intro-to-oo-in-ruby/starter_kit/run.rb
@@ -0,0 +1,7 @@
+require 'pry'
+
+
+
+
+binding.pry
+0
